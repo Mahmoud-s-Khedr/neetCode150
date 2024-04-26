845396960da3a0b682c60444201526defadafaf4
Mahmoud-s-Khedr <96141813+Mahmoud-s-Khedr@users.noreply.github.com>
2024-04-26 19:48:13 +0300

    Create containsDuplicate2.cpp

---
+++ containsDuplicate2.cpp
@@ -0,0 +1,10 @@
+    bool containsDuplicate(vector<int>& nums) {
+        map<int ,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]]==2){
+                return true;
+            }
+        }
+        return false;
+    }
